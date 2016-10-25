fcb0fd15a5f885f701532f6ce5b3bf300ae985c5
Qiang Wang <wangqiang2015@ia.ac.cn>
2016-10-25 22:51:48 +0800

    simple_goturn_4_vot

---
+++ simple_goturn_4_vot/regressor_regress.m
@@ -0,0 +1,20 @@
+function bbox_estimate = regressor_regress(net,curr_search_image,target_pad)
+
+%%convert from RGB to BGR and permute width and height
+curr_search_image = curr_search_image(:,:,[3,2,1]);
+curr_search_image = permute(curr_search_image,[2,1,3]);
+
+%%convert from RGB to BGR and permute width and height
+target_pad = target_pad(:,:,[3,2,1]);
+target_pad = permute(target_pad,[2,1,3]);
+
+target_ = imresize(single(target_pad), net.meta.normalization.imageSize(1:2),'METHOD','bilinear') ;
+target_ = bsxfun(@minus, target_, net.meta.normalization.averageImage) ;
+
+image_ = imresize(single(curr_search_image), net.meta.normalization.imageSize(1:2),'METHOD','bilinear') ;
+image_ = bsxfun(@minus, image_, net.meta.normalization.averageImage) ;
+
+net.eval({'target', target_,'image', image_}) ;
+
+bbox_estimate = (squeeze(net.vars(net.getVarIndex('fc8')).value));
+end
