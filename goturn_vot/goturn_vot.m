ab6610546b2cc034b9938ee2b6d3f12235cb0a15
Qiang Wang <wangqiang2015@ia.ac.cn>
2016-12-03 12:21:33 +0800

    fix

--- goturn_vot/goturn_vot.m
+++ goturn_vot/goturn_vot.m
@@ -16,9 +16,10 @@ RandStream.setGlobalStream(RandStream('mt19937ar', 'Seed', sum(clock)));
 % **********************************
 [handle, image, region] = vot('rectangle');
 
+addpath('../utils');
 vl_setupnn();
 % Initialize the tracker
-[state, ~] = goturn_initialize(vl_imreadjpeg({image}), region);
+[state, ~] = goturn_initialize(image, region);
 
 while true
     
@@ -32,7 +33,7 @@ while true
     end;
     
     % Perform a tracking step, obtain new region
-    [state, region] = goturn_update(state, vl_imreadjpeg({image}));
+    [state, region] = goturn_update(state, image);
     
     % **********************************
     % VOT: Report position for frame
@@ -48,23 +49,32 @@ handle.quit(handle);
 
 end
 
-function [state, location] = goturn_initialize(I, region, varargin)
-addpath('../utils');
+function [state, location] = goturn_initialize(image, region, varargin)
+
 state = [];
-state.visualization = true;
 state.gpu = ispc();
+state.visualization = true;
 
 state.net = dagnn.DagNN.loadobj(load('GOTURN_trained.mat'));
-state.net.layers(1).block.No = 1;
-state.net.vars(state.net.getVarIndex('image_target_crop')).precious = 1;
-state.net.vars(state.net.getVarIndex('image_search_crop')).precious = 1;
-if state.gpu,state.net.move('gpu');end;
 % state.net.mode = 'test';
+state.net.layers(1).block.No = 1;
+
+if state.gpu,state.net.move('gpu');end;
+if state.visualization
+    state.net.vars(state.net.getVarIndex('image_target_crop')).precious = 1;
+    state.net.vars(state.net.getVarIndex('image_search_crop')).precious = 1;
+end
+
 state.rect2bbox = @(x) ([x(1)-1,x(2)-1,x(1)-1+x(3),x(2)-1+x(4)]);
 state.bbox2rect = @(x) ([x(1)+1,x(2)+1,x(3)-x(1),x(4)-x(2)]);
 
-state.image_prev = I{1};
-state.sz = size(I{1});
+if state.gpu
+    state.image_prev = vl_imreadjpeg({image},'GPU');
+else
+    state.image_prev = vl_imreadjpeg({image});
+end
+
+state.sz = size(state.image_prev{1});
 state.rect_prev_tight = single(region);
 state.rect_prev_prior_tight = single(region);
 
@@ -72,7 +82,7 @@ location = region;
 
 if state.visualization
     figure;
-    subplot(1,3,1);state.h1 = imshow(uint8(I{1}));
+    subplot(1,3,1);state.h1 = imshow(uint8(state.image_prev{1}));
     state.h2 = rectangle('Position',location,'EdgeColor',[0,1,0]);
     subplot(1,3,2);state.h3 = imshow(zeros(227,227,3,'single'));
     subplot(1,3,3);state.h4 = imshow(zeros(227,227,3,'single'));
@@ -82,22 +92,26 @@ end
 
 end
 
-function [state, location] = goturn_update(state, I, varargin)
+function [state, location] = goturn_update(state, image, varargin)
 
-image_curr = I{1};
+if state.gpu
+    image_curr = vl_imreadjpeg({image},'GPU');
+else
+    image_curr = vl_imreadjpeg({image});
+end
+
 bbox_prev_tight = state.rect2bbox(state.rect_prev_tight);
 bbox_prev_prior_tight = state.rect2bbox(state.rect_prev_prior_tight);
 
 if state.gpu 
     state.net.eval({'bbox_target',gpuArray(bbox_prev_tight),...
     'bbox_search',gpuArray(bbox_prev_prior_tight),...
-    'image_target',gpuArray(state.image_prev),'image_search',gpuArray(image_curr)});
+    'image_target',state.image_prev{1},'image_search',image_curr{1}});
 else
     state.net.eval({'bbox_target',bbox_prev_tight,...
     'bbox_search',bbox_prev_prior_tight,...
-    'image_target',state.image_prev,'image_search',image_curr});
+    'image_target',state.image_prev{1},'image_search',image_curr{1}});
 end
-
 
 bbox_estimate = gather(squeeze(state.net.vars(state.net.getVarIndex('fc8')).value))';
 
@@ -125,7 +139,7 @@ location = double(rect_estimate_uncentered);
 if state.visualization
     scaled2rect = @(x) [x(1),x(2),x(3)-x(1),x(4)-x(2)]/10*227+1;
     
-    state.h1.set('CData',uint8(image_curr));
+    state.h1.set('CData',uint8(state.image_prev{1}));
     state.h2.set('Position',location);
     
     image_prev_crop = bsxfun(@plus,...
