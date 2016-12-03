7cb552d49ef23826c34c0e5323c79f2d5e4e5e8d
Qiang Wang <wangqiang2015@ia.ac.cn>
2016-12-03 09:07:25 +0800

    fitting

--- train/train_goturn.m
+++ train/train_goturn.m
@@ -6,9 +6,9 @@ opts.version = 1;
 opts.expDir = fullfile('..', 'data', ['GOTURN-experiment-' num2str(opts.version)]) ;
 opts.imdbPath = fullfile(opts.expDir, 'imdb.mat');
 
-trainOpts.learningRate = 1e-4;
+trainOpts.learningRate = [1e-4*ones(1,10),10e-5*ones(1,10)];
 trainOpts.weightDecay = 0.0005;
-trainOpts.numEpochs = 40;
+trainOpts.numEpochs = 20;
 trainOpts.batchSize = 1;
 opts.train = trainOpts;
 
